e4b46566bcc186d8d733492bfa4152b09d5e166d
galvaine <126985136+galvaine@users.noreply.github.com>
2025-05-04 10:52:51 -0300

    Update __init__.py

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -9,7 +9,10 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
 # Criando e configurando o banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:eNFmCW3zb4oxZY98@joylessly-sinewy-flatfish.data-1.use1.tembo.io:5432/postgres"
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'app.db')}"
 app.config['SECRET_KEY'] = '12456389'
 db = SQLAlchemy(app)
 
