cc8fd05befc71d50281f0e93da79346e06fa2c53
galvaine <126985136+galvaine@users.noreply.github.com>
2025-05-04 11:29:13 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,10 +1,1 @@
-#Importação da construção do aplicativo flask
-from api.app import app
-from api.app import db
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-
-
-    
+from app import app as application
