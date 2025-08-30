bd79608710fb312fd001bbc6a497d757424a42a7
galvaine <marcosgalileu12@gmail.com>
2025-08-30 18:15:17 -0300

    correção vercel

--- api/main.py
+++ api/main.py
@@ -1,8 +1,11 @@
 from app import app, db
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+# Cria o banco de dados ao iniciar (executado uma vez)
+with app.app_context():
+    db.create_all()
+
+# Função que o Vercel espera
+def handler(request):
+    return app(request.environ, lambda status, headers: (status, headers))
 
 
