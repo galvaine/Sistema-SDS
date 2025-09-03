fa5691ddeb7ada3405000deada81e97b457f1d66
galvaine <marcosgalileu12@gmail.com>
2025-09-03 12:15:17 -0300

    serie 002

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,5 +1,5 @@
 # Importação da construção do aplicativo para o arquivo rota
-from app import app
+from api.app import app
 from flask import Flask ,render_template,redirect, url_for,request
 from api.app.models import Cadastro, Permuta
 from api.app import db
