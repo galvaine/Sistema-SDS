3d1a467c84c640ca50b737af3920246049db6429
galvaine <marcosgalileu12@gmail.com>
2025-09-03 09:22:54 -0300

    criação do arquivo index

--- api/main.py
+++ api/main.py
@@ -1,16 +1,6 @@
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
-
-
 from app import app, db
 
-# Cria o banco de dados ao iniciar (executado uma vez)
-with app.app_context():
-    db.create_all()
-
-# Função que o Vercel espera
-def handler(request):
-    return app(request.environ, lambda status, headers: (status, headers))
-
-
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
